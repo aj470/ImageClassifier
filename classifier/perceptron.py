848e8b5050341d5744ef8677cc8639a362182c76
Nikhil <nikhilmathur95@gmail.com>
2018-04-29 21:46:36 -0400

    finished findHighWeightFeatures?

--- classifier/perceptron.py
+++ classifier/perceptron.py
@@ -7,12 +7,13 @@ class Perceptron(Classifier):
 	def __init__(self, legal_labels):
 		self.legal_labels = legal_labels
 		self.type = "perceptron"
-		self.weights = {}
+		self.weights = {} 
 		for label in legal_labels:
 			self.weights[label].util.Counter()
     @staticmethod
     def name():
         return "perceptron"
+		
 	def setWeights(self, weights):
 		assert len(weights) == len(self.legalLabels);
 		self.weights == weights;
@@ -24,14 +25,24 @@ class Perceptron(Classifier):
 		featuresWeights = []
 
 		"*** YOUR CODE HERE ***"
-		util.raiseNotDefined()
-
+		featuresWeights = self.weights[label].sortedKeys()[:100]
 		return featuresWeights		
       
     def train(self, training_data, validation_data):
         """Must return the final percentage accuracy achieved on validation data set."""
         raise NotImplemented()
 
-    def classify(self, data):
-        """Must return an array of predictions, each prediction corresponding to the image at that index."""
-        raise NotImplemented()
+	def classify(self, data ):
+		"""
+		Classifies each datum as the label that most closely matches the prototype vector
+		for that label.  See the project description for details.
+    
+		Recall that a datum is a util.counter... 
+		"""
+		guesses = []
+		for datum in data:
+			vectors = util.Counter()
+			for l in self.legalLabels:
+				vectors[l] = self.weights[l] * datum
+			guesses.append(vectors.argMax())
+		return guesses
