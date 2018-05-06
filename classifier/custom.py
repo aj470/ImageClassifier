60ed67dd77b921ccd90f00246231c224fe3a7053
Ayush <aj470@scarletmail.rutgers.edu>
2018-05-06 02:10:08 -0400

    Added Perceptron and KNN Classifier

--- classifier/custom.py
+++ classifier/custom.py
@@ -1,15 +1,114 @@
 from .classifier import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 
 class CustomClassifier(Classifier):
+    def __init__(self, legal_labels):
+        """KNN Classifier"""
+        self.legal_labels = legal_labels
+        self.legal_labels_len = len(legal_labels)
+        
     @staticmethod
     def name():
-        return "custom(mira?)"
+        return "custom - KNN"
+        
+    def _separate(self, dataset):
+        classes = {}
+        for data, cls in dataset:
+            if cls not in classes:
+                classes[cls] = []
+            classes[cls].append(data)
+        return classes
 
     def train(self, training_data, validation_data):
         """Must return the final percentage accuracy achieved on validation data set."""
-        raise NotImplemented()
+        #print(validation_data.get_labeled_images())
+        #training_dataset = {}
+        training_examples = 0
+        training_dataset = []
+        training_target = []
+        classes = self._separate(training_data.get_labeled_images())
+        for cls in classes:
+            images = classes[cls]               # images assigned to each class
+            flat_images = tuple(map(lambda x: x.flat_data(), images))  # flat images
+            for img in flat_images:
+                training_examples += 1
+                training_dataset.append(img)
+                training_target.append(cls)
+        training_dataset = np.array(training_dataset, dtype=np.int32)
+        training_target = np.array(training_target, dtype=np.int32)
+        
+        #print(training_examples)
+        #print(len(training_dataset),len(training_target))
+        #print(training_dataset[0])        
+        #print(self.legal_labels)
+            
+        
+        validation_dataset = []
+        validation_target = []
+        validation_examples = 0
+        classes = self._separate(validation_data.get_labeled_images())
+        for cls in classes:
+            images = classes[cls]               # images assigned to each class
+            flat_images = tuple(map(lambda x: x.flat_data(), images))  # flat images
+            for img in flat_images:
+                validation_examples += 1
+                validation_dataset.append(img)
+                validation_target.append(cls)
+        validation_dataset = np.array(validation_dataset, dtype=np.int32)
+        validation_target = np.array(validation_target, dtype=np.int32)
+        
+        
+        
+        
+        euclidean_distance_validation = np.zeros((validation_examples, training_examples), dtype=np.int32)
+        sorted_distance_class = np.zeros((validation_examples, training_examples), dtype=np.int32)
+        
+        # Calculates eculidian data for testing data
+        for i in range(0, validation_examples):
+            euclidean_distance_validation[i:i + 1, :] = np.sqrt(np.sum(np.square(training_dataset[:, :] - validation_dataset[i, :]), axis=1))
+        
+        
+        for j in range(0,validation_examples):
+           sorted_distance_class[j] = mylist = [training_target[i] for i in np.argsort(euclidean_distance_validation[j])]
+           #training_target[:][np.argsort(euclidean_distance_validation[j])]
+        
+        np.savetxt("label.csv", sorted_distance_class, delimiter=",")
+        np.savetxt("target.csv", validation_target, delimiter=",")
+        
+        k = [1,2,3,4,5,6,7,8,9,10]
+        print("K list", k)
+        testing_error = np.zeros((1, len(k)), dtype=np.float)
+        predicted_label = [] 
+        for item in k:
+            error = 0
+            for i in range(0, validation_examples):
+                lst = list(sorted_distance_class[i][0:item])
+                predicted_label.append(max(lst,key= lst.count))
+                if predicted_label[i] != validation_target[i]:
+                    error += 1
+            testing_error[0, k.index(item)] = error/validation_examples
+            
+
+        # plotting the graphs on same plane for errors vs value of K
+        plt.xlabel('Value of K')
+        plt.ylabel('Error')
+        # red represents the test error
+        # green represents the traning error
+        red_box = mpatches.Patch(color='red', label='Test Error')
+        green_box = mpatches.Patch(color='green', label='Traning Error')
+
+        plt.legend(handles=[red_box,green_box])
+
+        plt.plot(k, testing_error[0], color='red', marker='*')
+        plt.title('knn Classifier')
+        plt.show()
+       
+
+        return 
 
     def classify(self, data):
         """Must return an array of predictions, each prediction corresponding to the image at that index."""
-        raise NotImplemented()
+        #raise NotImplemented()
--- classifier/perceptron.py
+++ classifier/perceptron.py
@@ -1,7 +1,20 @@
+# perceptron.py
+# -------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
 from .classifier import *
+from .util import Counter
 import random
 import numpy as np
-from .util import Counter
 
 """ 
 We know the following:
@@ -11,8 +24,12 @@ face width = 60
 face height = 70
 """
 
-
 class Perceptron(Classifier):
+    """
+    Perceptron classifier.
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
+    """
     def __init__(self, legal_labels):
         self.legalLabels = legal_labels
         self.type = "perceptron"
@@ -31,70 +48,107 @@ class Perceptron(Classifier):
                 for column in range(70):  # height
                     for row in range(60):  # width
                         self.weights[label][(column, row)] = random.uniform(0, 1)
-
+                        
+    @staticmethod
+    def name():
+        return "perceptron"
+        
     def setWeights(self, weights):
-        assert len(weights) == len(self.legalLabels)
-        self.weights == weights
+        assert len(weights) == len(self.legalLabels);
+        self.weights = weights;
+
+    def _separate(self, dataset):
+        classes = {}
+        for data, cls in dataset:
+            if cls not in classes:
+                classes[cls] = []
+            classes[cls].append(data)
+        return classes
+
+    def train(self, training_data, validation_data):
+        """
+        The training loop for the perceptron passes through the training data several
+        times and updates the weight vector for each label based on classification errors.
+        See the project description for details.
+        Use the provided self.weights[label] data structure so that
+        the classify method works correctly. Also, recall that a
+        datum is a counter from features to values for those features
+        (and thus represents a vector a values).
+        """
+
+        classes = self._separate(training_data.get_labeled_images())
+        training_data = {}
+        k = 0
+        training_target = []
+        for cls in classes:
+            images = classes[cls]
+            flat_images = tuple(map(lambda x: x.flat_data(), images))
+            for image in flat_images: 
+                training_data[k] = image
+                training_target.append(cls)
+        training_target = np.array(training_target, dtype=np.int32)
+
+            
+        #self.features = training_data[0].keys() # could be useful later
+        # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
+        # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
+        lab = Counter()
+        for iteration in range(self.max_iterations):
+            print("Starting iteration ", iteration, "...")
+            for i in range(len(training_data)):
+                for l in self.legalLabels:
+                    #print(self.weights[l])
+                    lab[l] = self.weights[l].__mul__(training_data[i])
+                    #print(lab[l])
+                values = list(lab.values())
+                index = values.index(max(values,key= values.count))
+                keys = list(lab.keys())
+                
+                if not(training_target[i] == keys[index]):
+                    image_counter = Counter()
+                    count = 0
+                    for column in range(28):
+                        for row in range(28):
+                            image_counter[(column, row)] = training_data[i][count]
+                            count += 1
+                    #print(image_counter)
+                    self.weights[training_target[i]].__radd__(image_counter)
+                    self.weights[keys[index]].__sub__(image_counter)	           
+
+    def classify(self, data ):
+        """
+        Classifies each datum as the label that most closely matches the prototype vector
+        for that label.  See the project description for details.
+        Recall that a datum is a util.counter...
+        """
+        guesses = []
+        for image, label in data.get_labeled_images():
+            #print(image.flat_data())
+            vectors = Counter()
+            image_counter = Counter()
+            count = 0
+            for column in range(28):
+                for row in range(28):
+                    image_counter[(column, row)] = image.flat_data()[count]
+                    count += 1
+            for l in self.legalLabels:
+                vectors[l] = self.weights[l] * image_counter
+            values = list(vectors.values())
+            index = values.index(max(values,key= values.count))
+            keys = list(vectors.keys())
+            guesses.append(keys[index])
+        print("here")
+        print(guesses)
+        return guesses
+
 
     def findHighWeightFeatures(self, label):
         """
-		Returns a list of the 100 features with the greatest weight for some label
-		"""
-        featuresWeights = []
-
-        "*** YOUR CODE HERE ***"
-        top_amount = 100
-        featuresList = self.weights[label]
-        featuresWeights = featuresList.sortedKeys()[
-                          :top_amount]  # sort by values and then retrieve the top 100 features we want
-        return featuresWeights
-
-    def findScore(self, featureList, get_label):  # calculates score for label
-        score = featureList * self.weights[get_label]
-        return score
-
-    def findMaxScore(self, mydatum):
-        maxScoreLabel = self.legalLabels[0]
-        maxScore = self.findScore(mydatum, self.legalLabels[0])
-        i = 1
-        while i < len(self.legalLabels):
-            tempScore = self.findScore(mydatum, self.legalLabels[i])
-            if tempScore < maxScore:
-                pass
-            else:
-                maxScore = tempScore
-                maxScoreLabel = self.legalLabels[i]
-            i += 1
-        return maxScoreLabel
-
-    def train(self, training_data_set, validation_data_set):
-        """Must return the final percentage accuracy achieved on validation data set."""
-        training_data = [img.flat_data() for img in training_data_set.get_images()]
-        training_labels = training_data_set.get_labels()
-        for iteration in range(self.max_iterations):
-            print("Starting iteration ", iteration, "...")
-            indices = list(range(len(training_data)))
-            while len(indices) > 0:
-                datum_index = random.choice(indices)
-                indices.remove(datum_index)
-                training_datum = training_data[datum_index]
-                training_label = training_labels[datum_index]
-                temp_label = self.findMaxScore(training_datum)
-                if (temp_label != training_label):
-                    self.weights[training_label] = self.weights[training_label] + training_datum
-                    self.weights[temp_label] = self.weights[temp_label] - training_datum
-
-    def classify(self, data):
+        Returns a list of the 100 features with the greatest weight for some label
         """
-		Classifies each datum as the label that most closely matches the prototype vector
-		for that label.  See the project description for details.
-    
-		Recall that a datum is a util.counter... 
-		"""
-        guesses = []
-        for datum in data:
-            vectors = Counter()
-            for l in self.legalLabels:
-                vectors[l] = self.weights[l] * datum
-            guesses.append(vectors.argMax())
-        return guesses
+        featuresWeights = []
+        featuresWeights = self.weights[label].values()
+        "*** YOUR CODE HERE ***"
+        #util.raiseNotDefined()
+
+        return featuresWeights
